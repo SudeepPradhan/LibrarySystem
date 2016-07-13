7cb02913c797b23817fee8582a31632464065329
Sudeep Pradhan <pradhan_sudeep@hotmail.com>
2016-07-13 15:49:24 -0500

    missing file

---
+++ src/businesscontrollers/CirculationControllerImpl.java
@@ -0,0 +1,87 @@
+package businesscontrollers;
+
+import controllers.CirculationController;
+import database.DataManager;
+import database.DatabaseFacade;
+import java.time.LocalDate;
+import java.util.List;
+import businessmodels.Address;
+import models.business.LibraryMember;
+import Validation.LibraryMemberValidation;
+
+import businessmodels.CheckoutRecordEntry;
+import businessmodels.Inventory;
+import decorators.CustomerDecorator;
+import interfaces.Customer;
+
+public class CirculationControllerImpl implements CirculationController {
+
+    private DataManager dataManager = DatabaseFacade.getDataManager();
+
+    @Override
+    public CustomerDecorator searchLibraryMember(String memberId) {
+        return dataManager.getLibraryMember(memberId);
+    }
+
+    @Override
+    public List<CustomerDecorator> getLibraryMembers() {
+        return dataManager.getLibraryMembers();
+    }
+
+    @Override
+    public boolean checkout(String memberId, Inventory inventory) {
+        int borrowDuration = inventory.getProduct().getBorrowDuration();
+        LocalDate checkoutDate = LocalDate.now();
+        CustomerDecorator libraryMember = searchLibraryMember(memberId);
+        if (libraryMember == null) {
+            return false;
+        }
+        libraryMember.checkoutProduct(inventory, checkoutDate, checkoutDate.plusDays(borrowDuration));
+        if (dataManager.saveLibraryMember(libraryMember)) {
+            return dataManager.saveProduct(inventory.getProduct());
+        } else {
+            return false;
+        }
+    }
+
+    //Sudeep - need to remove
+    @Override
+    public boolean checkin(String memberId, CheckoutRecordEntry checkoutRecordEntry) {
+        LocalDate checkinDate = LocalDate.now();
+        CustomerDecorator libraryMember = searchLibraryMember(memberId);
+        if (libraryMember == null) {
+            return false;
+        }
+        libraryMember.returnProduct(checkoutRecordEntry, checkinDate);
+        if (dataManager.saveLibraryMember(libraryMember)) {
+            return dataManager.saveProduct(checkoutRecordEntry.getInventory().getProduct());
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean addLibraryMember(String memberId, String firstName, String lastName, Address address, String phoneNumber) {
+        CustomerDecorator libraryMember = new LibraryMember(memberId, firstName, lastName, address, phoneNumber);
+        if (libraryMember.validate(new LibraryMemberValidation()).isValid()) {
+            return dataManager.saveLibraryMember(libraryMember);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updateLibraryMember(String memberId, String firstName, String lastName, Address address, String phoneNumber) {
+        CustomerDecorator libraryMember = searchLibraryMember(memberId);
+        if (libraryMember == null) {
+            return false;
+        }
+        libraryMember.setFirstName(firstName);
+        libraryMember.setLastName(lastName);
+        libraryMember.setAddress(address);
+        libraryMember.setPhoneNumber(phoneNumber);
+        if (libraryMember.validate(new LibraryMemberValidation()).isValid()) {
+            return dataManager.saveLibraryMember(libraryMember);
+        }
+        return false;
+    }
+}
